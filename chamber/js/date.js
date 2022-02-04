f0d5e03dc78d0c35d40ff6bab9eed50961420156
kyeguy00 <91562786+kyeguy00@users.noreply.github.com>
2022-02-03 21:32:41 -0700

    Added everything for W05 assignment

--- chamber/js/date.js
+++ chamber/js/date.js
@@ -9,3 +9,14 @@ const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 
 datefield.innerHTML = `<div>${fulldate}</div>`;
 
+const today = new Date()
+let day_of_week;
+day_of_week = today.getDay();
+let message1;
+
+// if (day_of_week >= 1 && day_of_week <= 5) 
+if (day_of_week >= 1 && day_of_week <=2) {
+    message1 = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+}
+
+document.querySelector("#message1").textContent = message1
---
+++ chamber/js/weather.js
@@ -0,0 +1,18 @@
+// window.weatherWidgetConfig =  window.weatherWidgetConfig || [];
+//             window.weatherWidgetConfig.push({
+//                 selector:".weatherWidget",
+//                 apiKey:"U3HLFFHLKTQLEEAHWFSWGBY6D", //Sign up for your personal key
+//                 location:"Kanab, UT", //Enter an address
+//                 unitGroup:"us", //"us" or "metric"
+//                 forecastDays:5, //how many days forecast to show
+//                 title:"Kanab, UT", //optional title to show in the 
+//                 showTitle:true, 
+//                 showConditions:true
+//             });
+           
+//             (function() {
+//             var d = document, s = d.createElement('script');
+//             s.src = 'https://www.visualcrossing.com/widgets/forecast-simple/weather-forecast-widget-simple.js';
+//             s.setAttribute('data-timestamp', +new Date());
+//             (d.head || d.body).appendChild(s);
+//             })();
