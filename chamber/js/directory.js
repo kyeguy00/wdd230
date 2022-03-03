15ee8b15c91537eea6495aa6755e961a0d288773
kyeguy00 <91562786+kyeguy00@users.noreply.github.com>
2022-03-02 22:29:26 -0700

    Added json file part 2

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -1,2 +1,27 @@
-const data = require('./data.json');
-console.log(data);
+const requestURL = "https://github.com/kyeguy00/wdd230/blob/5c686b2cd9cf5fc1335e189d05f499e51c652fc6/chamber/js/data.json";
+const cards = document.querySelector('.cards');
+
+
+fetch(requestURL)
+    .then(function (response){
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        const businesses = jsonObject['businesses'];
+        displayProphets.forEach(displayBusinesses);
+    })
+
+
+
+function displayBusinesses(business) {
+
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let image = document.createElement('img');
+    let phone = document.createElement('p');
+    let address = document.createElement('p');
+
+    h2.textContent = prophet.name
+
+    card.appendChild(h2);
+}
